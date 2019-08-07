6181d91f4e388e31c6f639521bb24227ddc40542
chenxinmeng-123 <aa@qq.com>
2019-08-07 14:25:50 +0800

    1

--- test1/test1.py
+++ test1/test1.py
@@ -3,5 +3,6 @@ driver=webdriver.Chrome()
 driver.maximize_window()
 
 driver.implicitly_wait(10)
-driver.get(http://chat.shopee.com/#/login?redirect=%2Ffans%2Flist)
+
+driver.get('http://chat.shopee.com/#/dashboard')
 
