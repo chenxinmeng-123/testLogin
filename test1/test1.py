224c8e7564a30660b4a355dc7aba95f6e81f19fd
chenxinmeng-123 <aa@qq.com>
2019-08-07 15:00:08 +0800

    login

--- test1/test1.py
+++ test1/test1.py
@@ -5,4 +5,19 @@ driver.maximize_window()
 driver.implicitly_wait(10)
 
 driver.get('http://chat.shopee.com/#/dashboard')
+'''
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[3]/div/button').click()
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[3]/div/button').send_keys('123')
+'''
 
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[1]/div/div[1]/input').clear()
+driver.implicitly_wait(1)
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[2]/div/div[1]/input').clear()
+driver.implicitly_wait(1)
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[1]/div/div[1]/input').click()
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[1]/div/div[1]/input').send_keys(chenxinmeng)
+driver.implicitly_wait(1)
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[2]/div/div[1]/input').click()
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[2]/div/div[1]/input').send_keys('123654')
+driver.implicitly_wait(1)
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[3]/div/button').click()
